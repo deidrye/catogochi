751ddb9a4da535f2282950d5d3e304c3a8dacc89
doktopbahtyc <denisoid93@gmail.com>
2025-04-24 19:17:16 +0300

    fix

--- server/src/middlewares/cors.js
+++ server/src/middlewares/cors.js
@@ -1,35 +1,10 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin(origin, callback) {
-    // В режиме разработки разрешаем все origin'ы
-    if (process.env.NODE_ENV === 'development') {
-      return callback(null, true);
-    }
-
-    // В продакшене разрешаем только определенные origin'ы
-    const allowedOrigins = [
-      'http://localhost:8081',
-      'http://localhost:3000',
-      'exp://localhost:8081',
-      'exp://192.168.1.1:8081',
-      'http://10.0.2.2:8081',
-      'http://10.0.2.2:3000',
-      'http://192.168.0.101:3000', // Добавь IP своего компа
-      'http://192.168.0.101:8081',
-      'exp://192.168.0.101:8081',
-    ];
-
-    // Разрешаем запросы без origin (например, из мобильного Expo Go)
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-
-    return callback(new Error('Not allowed by CORS'));
-  },
+  origin: true, // Разрешаем все origin'ы
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 };
 
 module.exports = cors(corsOptions);
--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.SERVER_PORT || 3000;
-const HOST = '0.0.0.0'; // Слушаем на всех интерфейсах
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server has started on ${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });
