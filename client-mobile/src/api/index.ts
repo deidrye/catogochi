28138548a19896c53dba84bae3cf82559c9df636
deidrye <ananastasyuk@mail.ru>
2025-04-25 10:15:37 +0300

    rebased auth and cors settings

--- client-mobile/src/api/index.ts
+++ client-mobile/src/api/index.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import { CLIENT_IP } from '@env';
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  CLIENT_IP: z.string().ip(),
+});
+
+export const env = envSchema.parse({ CLIENT_IP });
 
 // Для Android эмулятора используем 10.0.2.2 вместо localhost
 // Для iOS симулятора используем localhost
 // Для физического устройства используем IP компьютера
 const API_URL = __DEV__
-  ? 'http://172.18.42.82:3000/api' // IP вашего компьютера в локальной сети
+  ? `http://${env.CLIENT_IP}:3000/api` // IP вашего компьютера в локальной сети
   : 'http://localhost:3000/api';
 
 const api = axios.create({
